c6d2cfedc814879e5bfdf4c24d663bd12b6e1551
Priyanshu Shukla <44258359+priyanshu3666@users.noreply.github.com>
2020-02-24 22:35:09 +0530

    Update Implementation.java

--- src/Implementation.java
+++ src/Implementation.java
@@ -1,18 +1,27 @@
-
 public class Implementation {
     public static void main(String[] args) {
         Methods M = new Methods();
         int choice;
         while (true) {
             choice = M.menu();
-            if (choice == 1) {
-                M.insert();
-            } else if (choice == 2) {
-                M.displayAll();
-                break;
+
+            switch (choice) {
+                case 1:
+                    M.insert();
+                    break;
+                case 2:
+                    M.displayAll();
+                    break;
+                case 3:
+                    M.search();
+                    break;
+                case 4:
+                    M.delete();
+                    break;
+                case 5:
+                    M.exit();
+                    break;
             }
         }
     }
 }
-
-
