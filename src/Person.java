39526c2c33d191b5f1fdaa360df487dfe7713dc5
Priyanshu Shukla <44258359+priyanshu3666@users.noreply.github.com>
2020-02-24 22:37:53 +0530

    Update Person.java

--- src/Person.java
+++ src/Person.java
@@ -4,15 +4,15 @@ public class Person {
     String[] phone;
     String email;
     Person next;
+    char priority;
 
     Person(String first, String last, String[] phone, String email) {
-        this.first = first;
-        this.last = last;
+        this.first = first.toUpperCase();
+        this.last = last.toUpperCase();
         this.phone = phone;
-        this.email = email;
+        this.email = email.toLowerCase();
+        priority = (char) first.toUpperCase().charAt(0);
         next = null;
 
     }
-
-
 }
